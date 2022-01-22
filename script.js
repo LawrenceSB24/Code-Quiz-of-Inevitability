0a4d6f3061b3b28c0614e8967c446ef112d4863a
LawrenceSB24 <lawrs2022@gmail.com>
2022-01-22 17:41:20 -0500

    Fixed user choice selection so that time is not decremented when correct answer is picked

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Variable that selects the start button based on its id
 var startBtn = document.getElementById("strt-btn");
-var restartBtn = document.getElementById("rst-btn")
+// var restartBtn = document.getElementById("rst-btn")
 
 // Variable that focus on the container for the quiz
 var quizBox = document.getElementById("question-container");
@@ -15,6 +15,9 @@ var answerB = document.getElementById("B");
 var answerC = document.getElementById("C");
 var answerD = document.getElementById("D");
 
+
+var scoring = document.getElementById("highscore");
+
 // Variable that targets the timer element
 var TimerEl = document.getElementById("timer");
 
@@ -22,29 +25,32 @@ var TimerEl = document.getElementById("timer");
 var Begin = document.getElementById("intro");
 
 
-// Variables that target the initials and highscore elements
-var Inits = document.getElementById("initials");
-var HS = document.getElementById("highscore");
 
+//  Event listerners for the answer buttons
+answerA.addEventListener('click', comparision);
+answerB.addEventListener('click', comparision);
+answerC.addEventListener('click', comparision);
+answerD.addEventListener('click', comparision);
 
 // counter variable for the current question by incrementing or decrementing said counter
 // based on interactivity of buttons.
 // var shuffle;
 var current_Q = 0;
-var currentQuestion;
-
 
 // counter for score. For correct answers, the counter will be incremented. For incorrect answers
 // score will be decremented.
-var score = 0;
+var score;
+
+// Highscore variable to store the final score.
+var HighS;
 
 // counter for correct choices
-var correct_Q = 0;
+var correct_Q;
 
 // default counters for timer function
-let seconds;
+var seconds;
 // Array that will contain questions and answers. Starting with one question as base line.
-// Potential total of 10-20 questions.
+
 
 var questions = [
     {
@@ -53,7 +59,7 @@ var questions = [
         b: "To develop the software for computers.",
         c: "To make footlong xl sandwiches.",
         d: "To eat user data in form of chicken noodle soup.",
-        correct: "To create the backbone of a website."
+        correct: "A"
     },
 
     {
@@ -62,7 +68,7 @@ var questions = [
         b: "<span></span>",
         c: "<src></src>",
         d: "<script></script>",
-        correct: "<script></script>"
+        correct: "D"
     },
 
     {
@@ -71,7 +77,7 @@ var questions = [
         b: "Javascript Object Notation",
         c: "Jumping sausage orca name",
         d: "Hyper Tacos Markup Language",
-        correct: "Hypertext Markup Language"
+        correct: "B"
     },
 
     {
@@ -80,7 +86,7 @@ var questions = [
         b: "ask for its number",
         c: "people {function()}",
         d: "function people() {}",
-        correct: "function people() {}"
+        correct: "D"
     },
 
     {
@@ -89,7 +95,7 @@ var questions = [
         b: "speak()",
         c: "prompt()",
         d: ":talk",
-        correct: "prompt()"
+        correct: "C"
     },
 
     {
@@ -98,7 +104,7 @@ var questions = [
         b: "== looks at values, === looks at values and types",
         c: "both look at types",
         d: "one looks at types, one looks at values",
-        correct: "== looks at values, === looks at values and types"
+        correct: "B"
     },
 
     {
@@ -107,7 +113,7 @@ var questions = [
         b: "Not-a-Number",
         c: "Nuts-always-Near",
         d: "Nokia-and-Nike",
-        correct: "Not-a-Number"
+        correct: "B"
     },
 
     {
@@ -116,7 +122,7 @@ var questions = [
         b: "nothing",
         c: "something",
         d: "Refers to the object that the function is a property of",
-        correct: "Refers to the object that the function is a property of"
+        correct: "D"
     },
 
     {
@@ -125,7 +131,7 @@ var questions = [
         b: "Booleans",
         c: "all of the above",
         d: "Object",
-        correct: "all of the above"
+        correct: "C"
     },
 
 ];
@@ -139,73 +145,82 @@ function beginQuiz() {
     console.log("Game has begun");
     Begin.classList.add("hide");
     quizBox.classList.remove("hide");
+    score = 0;
+    HighS = [];
+    current_Q= 0;
     show_Q();
     time();
 }
 
 
 function show_Q() {
-    currentQuestion = questions[current_Q];
-    quizQ.textContent = currentQuestion.question;
-    answerA.textContent = currentQuestion.a;
-    answerB.textContent = currentQuestion.b;
-    answerC.textContent = currentQuestion.c;
-    answerD.textContent = currentQuestion.d;
-
-
-    answerA.addEventListener('click', comparision);
-    answerB.addEventListener('click', comparision);
-    answerC.addEventListener('click', comparision);
-    answerD.addEventListener('click', comparision);
-
+    quizQ.textContent = questions[current_Q].question;
+    answerA.textContent = questions[current_Q].a;
+    answerB.textContent = questions[current_Q].b;
+    answerC.textContent = questions[current_Q].c;
+    answerD.textContent = questions[current_Q].d;
+    scoring.innerText = score;
 }
+
+
 
 function time() {
     seconds = 120;
     timerInterval = setInterval(function () {
         seconds--;
         TimerEl.innerText = "Timer: " + seconds;
-        if (seconds < 0 || current_Q === questions[current_Q].length) {
+        if (seconds === 0) {
+            clearInterval(timerInterval);
             End();
         }
     }, 1000);
+
+    console.log(End)
 }
 
 function End() {
-    clearInterval(timerInterval);
-    currentQuestion = 0;
     quizBox.classList.add("hide");
     highscore();
 }
 
 function comparision(event) {
-    if (event.target.matches === currentQuestion.correct) {
+
+    if (event) {
+        console.log(event.target);
+        console.log(event.target.id);
+    }
+    if (event.target.id === questions[current_Q].correct) {
         console.log("correct!");
-        correct_Q++;
         score++;
-        current_Q++;
         TimerEl.innerText = "Timer: " + seconds;
-    } 
-    
-    if (event.target.matches !== currentQuestion.correct) {
+        console.log(score);
+    } else {
         console.log("incorrect.");
         seconds -= 5;
         TimerEl.innerText = "Timer: " + seconds;
-        current_Q++;
+        TimerC();
     }
 
-    if (current_Q < questions.length) {
-        show_Q();
-    } else {
+    if (questions[current_Q].question == "What data types are included in JS?") {
         End();
+    } else {
+        current_Q++;
+        show_Q();
     }
 }
 
 
 function highscore() {
-    Inits.classList.remove("hide");
-    HS.classList.remove("hide");
-    initial = prompt("Please enter your initials: ");
-    Inits.innerText = initial;
-    HS.innerText = "Highscore: " + score;
+    initial = window.prompt("Please enter your initials: ");
+    HighS = {
+        Init: initial,
+        score: score
+    }
+}
+
+function TimerC() {
+    if (seconds <= 0) {
+        clearInterval(seconds);
+        End();
+    }
 }
